26deba4a23352a2e9802b4af05eed7c2288ac7b4
Oscar F <ofalkman@gmail.com>
2016-05-03 16:59:13 +0200

    CardPayment and Payment all clear.

--- se.biluthyrning/src/model/CardPayment.java
+++ se.biluthyrning/src/model/CardPayment.java
@@ -5,10 +5,11 @@ public class CardPayment {
 		
 	}
 	public CardPayment (Payment paymentDetails) {
-		}
 	
-		public 	boolean checkPayment (PaymentDTO payment) {
-			boolean checked = externalSystems.ExternalPaymentAuthorization.authorizePayment(payment);
+	}
+	
+	public boolean checkPayment (PaymentDTO payment) {
+		boolean checked = externalSystems.ExternalPaymentAuthorization.authorizePayment(payment);
 		return checked;
 	}
 }
