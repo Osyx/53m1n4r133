42812923b6d1867314b55b12448c2d782e55cd48
Joakim <Joakim@Joakim-PC>
2016-05-03 14:47:48 +0200

    Methods CREATED

--- se.biluthyrning/src/controller/Controller.java
+++ se.biluthyrning/src/controller/Controller.java
@@ -1,6 +1,49 @@
+package controller;
 
 public class Controller {
 	
-	public
+	/**
+	 * 
+	 */
+	public void Controller(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void handleResults(ResultDTO resultDTO){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public PaymentDTO payment(PaymentDTO paymentDTO){
+		
+		return paymentDTO;
+	}
+	
+	/**
+	 * 
+	 */
+	public Inspection newInspection(String regNumber){
+		
+		return inspection;
+	}
+	
+	/**
+	 * 
+	 */
+	public void nextQueueNumber(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void closeDoor(){
+		
+	}
 
 }
