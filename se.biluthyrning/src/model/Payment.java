ea03630c9c6e92d6e09dadd8e30f0e4e1ea3229c
Oscar F <ofalkman@gmail.com>
2016-05-03 16:59:30 +0200

    Payment done.

--- se.biluthyrning/src/model/Payment.java
+++ se.biluthyrning/src/model/Payment.java
@@ -4,16 +4,33 @@ public class Payment {
 	private PaymentDTO paymentDetails;
 	private int change = 0;
 	private boolean paymentSuccessful;
+	CardPayment cardPayment = new CardPayment();
 	
 	public Payment(PaymentDTO payment) {
 		this.paymentDetails = payment;
-		this.change = 0;
-		this.paymentSuccessful = false;
+		this.setChange(0);
+		this.setPaymentSuccessful(false);
 		
 		if (paymentDetails.getCard() == true) {
-		paymentSuccessful = CardPayment.authorizePayment(payment); 
+		setPaymentSuccessful(cardPayment.checkPayment(payment)); 
 				
 		}
+	}
+
+	public boolean isPaymentSuccessful() {
+		return paymentSuccessful;
+	}
+
+	private void setPaymentSuccessful(boolean paymentSuccessful) {
+		this.paymentSuccessful = paymentSuccessful;
+	}
+
+	public int getChange() {
+		return change;
+	}
+
+	public void setChange(int change) {
+		this.change = change;
 	}
 }
 
