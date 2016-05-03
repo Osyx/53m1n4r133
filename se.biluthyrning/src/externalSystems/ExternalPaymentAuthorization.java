f6e0660d285463bf7f47e9298d0b9d9fd18cd1cc
Tiberians <silverskiold@gmail.com>
2016-05-03 16:36:37 +0200

    Pröva

---
+++ se.biluthyrning/src/externalSystems/ExternalPaymentAuthorization.java
@@ -0,0 +1,9 @@
+package externalSystems;
+
+import model.PaymentDTO;
+
+public class ExternalPaymentAuthorization {
+	public static boolean authorizePayment(PaymentDTO paymentDetails) {
+		return true;
+	}
+}
