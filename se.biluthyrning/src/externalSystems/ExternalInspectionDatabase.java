b6664cb0f3fb5e67a5f4486c526e5307d24218fa
Tiberians <silverskiold@gmail.com>
2016-05-04 14:10:56 +0200

    Commenting EPA

--- se.biluthyrning/src/externalSystems/ExternalInspectionDatabase.java
+++ se.biluthyrning/src/externalSystems/ExternalInspectionDatabase.java
@@ -6,25 +6,7 @@ public class ExternalInspectionDatabase {
 	
 	public static List<String> getInspectionInstructions(String regnr){
 		
-		addInspectionItemsToList(regnr);
 		return inspectionObjects;
 	}
-	
-	/**
-	 * This is just a pseudo-database. It will NOT return any results based on the actual registration number of the car. The return list will be the same for all parameters.
-	 *
-	 * @param regnr will not be used in this test
-	 */
-	public static void addInspectionItemsToList(String regnr){
-		inspectionObjects.add("Seat belts: Inspect for proper operation and anchorage.");
-		inspectionObjects.add("Brakes: Remove one of the front wheels and check the condition of the disc brake pads.");
-		inspectionObjects.add("Steering: Check the condition of the front end assembly.");
-		inspectionObjects.add("Tires: Check the tread depth, tire condition and tire pressure.");
-		inspectionObjects.add("Lights: Make sure that all the lights are working correctly.");
-		inspectionObjects.add("Windshield: Inspect for cracks. Shouldn´t be longer than 11 inches.");
-		inspectionObjects.add("Horn: Check mounting and operation.");
-		inspectionObjects.add("Mirrors: Check mirror locations for proper mounting, cracks, breaks, and/or discoloration.");
-		inspectionObjects.add("Fuel Leaks: Make sure that there is no fuel leak.");
-	}
-	
+
 }
