a6c13fd6b4d48e80d305159d2e088df08078805d
Oscar F <ofalkman@gmail.com>
2016-05-04 17:27:01 +0200

    ReportDTO comments.

--- se.biluthyrning/src/model/ReportDTO.java
+++ se.biluthyrning/src/model/ReportDTO.java
@@ -2,16 +2,29 @@ package model;
 import java.util.*;
  
 public class ReportDTO{
+	/**
+	 * Contains a list of element, later in the program filled with the results.
+	 */
 	List<String> reportList = new ArrayList<>();
  	
+	/**
+	 * Constructor for <code>ReportDTO</code>.
+	 * @param resultList The results we want to save into the <code>ReportDTO</code>
+	 */
  	public ReportDTO(List<String> resultList){
  		
  	}
- 	
+ 	/**
+ 	 * Setter for <code>reportList</code>.
+ 	 * @param resultList The list adress we want instead.
+ 	 */
  	public void createNewReport(List<String> resultList){
- 		reportList = resultList;
+ 		this.reportList = resultList;
  	}
- 	
+ 	/**
+ 	 * Getter for <code>reportList</code>
+ 	 * @return reportList
+ 	 */
  	public List<String> getReportList(){
  		return this.reportList;
  	}
