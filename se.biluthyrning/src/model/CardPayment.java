183c3c1d9b7bb173f7500f77c1659506ad61effc
Tiberians <silverskiold@gmail.com>
2016-05-04 13:28:13 +0200

    Added comments to CardPayment

--- se.biluthyrning/src/model/CardPayment.java
+++ se.biluthyrning/src/model/CardPayment.java
@@ -1,5 +1,8 @@
 package model;
-
+/*
+ * This class serves the purpose of handling cardpayments, as well
+ * as the interaction with the ExternalPaymentAuthorization.
+ */
 public class CardPayment {
 	boolean approval;
 	
@@ -11,7 +14,12 @@ public class CardPayment {
 	public boolean getApproval() {
 		return this.approval;
 	}
-	
+	/*
+	 * This method serves as the interaction method with the ExternPaymentAuthorization
+	 * and checks whether or not it is approves the card details the customer has
+	 * provided.
+	 * @return true for a payment that has been approved, otherwise false.
+	 */
 	private boolean approvePayment(PaymentDTO paymentDetails) {
 		return externalSystems.ExternalPaymentAuthorization.authorizePayment(paymentDetails);
 		
