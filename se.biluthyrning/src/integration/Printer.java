842a436ce63fe320b9de437b2314e77b0903180b
Oscar F <ofalkman@gmail.com>
2016-05-04 15:05:26 +0200

    Comments on Printer.

--- se.biluthyrning/src/integration/Printer.java
+++ se.biluthyrning/src/integration/Printer.java
@@ -1,10 +1,16 @@
 package integration;
 
 public class Printer {
+	/**
+	 * Constructor for <code>Printer</code>.
+	 */
 	public Printer(){
 		
 	}
-	
+	/**
+	 * Sends the argument to the printer.
+	 * @param obj the object we want to print.
+	 */
 	public void Print(String obj){
 		System.out.println("Debug: Printing " + obj);
 	}
