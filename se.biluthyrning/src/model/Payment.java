9ae436fb1001e22d39e1c3202c5c4da5327c4199
Tiberians <silverskiold@gmail.com>
2016-05-04 11:04:19 +0200

    Changed the setPayment function, intitializes CardPayment within method

--- se.biluthyrning/src/model/Payment.java
+++ se.biluthyrning/src/model/Payment.java
@@ -4,27 +4,38 @@ public class Payment {
 	private PaymentDTO paymentDetails;
 	private int change = 0;
 	private boolean paymentSuccessful;
-	CardPayment cardPayment = new CardPayment();
 	
+	
+	/**
+	 * 
+	 * Takes @param payment and creates a new object that includes change
+	 * and whether or not the the payment was approved or not.
+	 */
 	public Payment(PaymentDTO payment) {
 		this.paymentDetails = payment;
 		this.setChange(0);
 		this.paymentSuccessful = false;
 		
 		if (paymentDetails.getCard() == true) {
+			
 			setPaymentSuccessful(payment); 
 				
 		}
 	}
 /**
  * 
- * @return whether or not the payment is succesful or not
+ * @return whether or not the payment is successful or not
  */
 	public boolean isPaymentSuccessful() {
 		return paymentSuccessful;
 	}
 
+	/**
+	 * 
+	 * @param paymentDetails
+	 */
 	private void setPaymentSuccessful(PaymentDTO paymentDetails) {
+		CardPayment cardPayment = new CardPayment();
 		this.paymentSuccessful = cardPayment.checkPayment(paymentDetails);
 	}
 
