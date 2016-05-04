05885decf20f23e94803eaf7c7185074a724ee71
KN4S <jmarken@kth.se>
2016-05-04 11:08:25 +0200

    ReportDTO recreated

---
+++ se.biluthyrning/src/model/ReportDTO.java
@@ -0,0 +1,19 @@
+package model;
+import java.util.*;
+ 
+public class ReportDTO{
+	List<String> reportList = new ArrayList<>();
+ 	
+ 	public ReportDTO(){
+ 		
+ 	}
+ 	
+ 	public void createNewReport(List<String> resultList){
+ 		
+ 	}
+ 	
+ 	public List<String> getReportList(){
+ 		return this.reportList;
+ 	}
+ 
+ }
