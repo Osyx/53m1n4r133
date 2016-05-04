2c23cbfcb70f9b7b9e99e34046b161fe445ce180
Oscar F <ofalkman@gmail.com>
2016-05-04 14:41:04 +0200

    Comments on Garage.

--- se.biluthyrning/src/integration/Garage.java
+++ se.biluthyrning/src/integration/Garage.java
@@ -1,17 +1,28 @@
 package integration;
 
 public class Garage {
+	/**
+	 * Creates instance of the <code>GarageDoor</code> and the <code>Display</code>.
+	 */
 	integration.Garagedoor garageDoor = new integration.Garagedoor();
 	integration.Display display = new integration.Display();
 	
+	/**
+	 * Constructor for the <code>Garage</code>.
+	 */
 	public Garage(){
 		
 	}
-	
+	/**
+	 * Closes the door by calling <code>garageDoor</code>'s function <code>closeDoor()</code>.
+	 */
 	public void closeDoor(){
 		garageDoor.closeDoor();
 	}
-	
+	/**
+	 * Prepares for the next customer by opening the door and showing the queue number. 
+	 * Does this by calling on <code>garageDoor</code> with <code>openGarageDoor()</code> and calling <code>display</code> with the function <code>displayNextNumber</code>.
+	 */
 	public void nextCustomer(){
 		garageDoor.openGarageDoor();
 		display.displayNextNumber();
