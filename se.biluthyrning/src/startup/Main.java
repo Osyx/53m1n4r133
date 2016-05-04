135332ac8f8104dbce1aad087a712d3da23f4f12
Oscar F <ofalkman@gmail.com>
2016-05-04 11:14:29 +0200

    Fixed main.

--- se.biluthyrning/src/startup/Main.java
+++ se.biluthyrning/src/startup/Main.java
@@ -2,17 +2,18 @@ package startup;
 
 import view.View;
 import controller.Controller;
+import model.Payment;
 import model.PaymentCreator;
 import integration.*;
 import dbHandler.RegistryCreator;
 
 public class Main {
 	public static void main(String[] args){
-		PaymentCreator paymentCreator = new PaymentCreator();
+		
 		RegistryCreator registryCreator = new RegistryCreator();
 		Printer printer = new Printer();
 		Garage garage = new Garage();
-		Controller controller = new Controller(paymentCreator, registryCreator, printer, garage);
+		Controller controller = new Controller(registryCreator, printer, garage);
 		View view = new View(controller);
 		
 		while(true)
