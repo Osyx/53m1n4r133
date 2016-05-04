9cb7f8bf87dad8c9d3aa516862d0deaf18b89589
Oscar F <ofalkman@gmail.com>
2016-05-04 14:42:50 +0200

    Updated comment on Garage

--- se.biluthyrning/src/integration/Garage.java
+++ se.biluthyrning/src/integration/Garage.java
@@ -21,7 +21,8 @@ public class Garage {
 	}
 	/**
 	 * Prepares for the next customer by opening the door and showing the queue number. 
-	 * Does this by calling on <code>garageDoor</code> with <code>openGarageDoor()</code> and calling <code>display</code> with the function <code>displayNextNumber</code>.
+	 * Does this by calling on <code>garageDoor</code> with <code>openGarageDoor()</code> 
+	 * and calling <code>display</code> with the function <code>displayNextNumber</code>.
 	 */
 	public void nextCustomer(){
 		garageDoor.openGarageDoor();
