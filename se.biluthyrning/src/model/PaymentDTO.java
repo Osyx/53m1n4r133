71aa61a5e8059a64041f9dbc78df9e703027408f
Tiberians <silverskiold@gmail.com>
2016-05-05 10:46:03 +0200

    Comments on PaymentDTO

--- se.biluthyrning/src/model/PaymentDTO.java
+++ se.biluthyrning/src/model/PaymentDTO.java
@@ -33,31 +33,54 @@ public class PaymentDTO {
 		this.code = code;
 		this.cardHolder = cardHolder;
 	}
-	
+	/**
+	 * 
+	 * @return <code>true</code> whether or not the customer is paying with card
+	 * 		   <code>false</code> if the customer is paying with cash
+	 */
 	public boolean getCard(){
 		return this.card;
 	}
-	
+	/**
+	 * 
+	 * @return the amount the customer is paying
+	 */
 	public int getamount(){
 		return this.amount;
 	}
 	
+	/**
+	 * 
+	 * @return The card number of the customer's card
+	 */
 	public int getCardNumber(){
 		return this.cardNumber;
 	}
-	
+	/**
+	 * 
+	 * @return The CVC number belonging to the customer's card
+	 */
 	public int getCVC(){
 		return this.cvc;
 	}
-
+	/**
+	 * 
+	 * @return The expiry date of the customer's card
+	 */
 	public int getExpiry(){
 		return this.expiry;
 	}
-	
+	/**
+	 * 
+	 * @return The code for the customer's card
+	 */
 	public int getCode(){
 		return this.code;
 	}
-	
+	/**
+	 * 
+	 * @return The name of the cardholder
+	 */
 	public String getCardHolder(){
 		return this.cardHolder;
 	}
