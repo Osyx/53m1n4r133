ce9653e87a52dedc12a59f5aa79fbb7204e0fc9d
Oscar F <ofalkman@gmail.com>
2016-05-05 15:50:49 +0200

    Inspections tests are done.

--- se.biluthyrning/src/test/model/InspectionTest.java
+++ se.biluthyrning/src/test/model/InspectionTest.java
@@ -4,16 +4,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.model.Inspection;
+
 public class InspectionTest {
 
 	@Test
 	public void testInspection() {
-		fail("Not yet implemented");
+		int lowestValue = 0;
+		for(int i = 9999; i > -1; i--) {
+			Inspection inspection = new Inspection("ABC 123");
+			assertNotEquals("No inspection created", inspection, null);
+			if(inspection.getCost() < lowestValue)
+				lowestValue = inspection.getCost();
+			if(lowestValue < 0) {
+				lowestValue = -1;
+				break;
+			}
+		}
+		
+		assertNotEquals("Cost wrongly calculated", lowestValue, -1);
 	}
 
 	@Test
-	public void testSetInspectionList() {
-		fail("Not yet implemented");
+	public void testFetchInspectionList() {
+		for(int i = 9999; i > -1; i--){
+			Inspection inspection = new Inspection("ABC 123");
+			assertEquals("Inspection result contains nothing", inspection.getInspectionList().isEmpty(), false);
+		}
 	}
 
 }
