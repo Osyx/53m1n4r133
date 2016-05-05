62eadc90f82aa27c90a425d1d98c3dc3a8957517
KN4S <jmarken@kth.se>
2016-05-05 11:04:36 +0200

    testers created

---
+++ se.biluthyrning/src/controller/ControllerTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Joakim
+ *
+ */
+public class ControllerTest {
+
+	/**
+	 * Test method for {@link controller.Controller#Controller(dbHandler.RegistryCreator, integration.Printer, integration.Garage)}.
+	 */
+	@Test
+	public void testController() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.Controller#handleResult(model.ReportDTO)}.
+	 */
+	@Test
+	public void testHandleResult() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.Controller#payment(model.PaymentDTO)}.
+	 */
+	@Test
+	public void testPayment() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.Controller#newInspection(java.lang.String)}.
+	 */
+	@Test
+	public void testNewInspection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.Controller#nextQueueNumber()}.
+	 */
+	@Test
+	public void testNextQueueNumber() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.Controller#closeDoor()}.
+	 */
+	@Test
+	public void testCloseDoor() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ se.biluthyrning/src/externalSystems/ExternalInspectionDatabaseTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package externalSystems;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Joakim
+ *
+ */
+public class ExternalInspectionDatabaseTest {
+
+	/**
+	 * Test method for {@link externalSystems.ExternalInspectionDatabase#getInspectionInstructions(java.lang.String)}.
+	 */
+	@Test
+	public void testGetInspectionInstructions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link externalSystems.ExternalInspectionDatabase#addInspectionItemsToList(java.lang.String)}.
+	 */
+	@Test
+	public void testAddInspectionItemsToList() {
+		fail("Not yet implemented");
+	}
+
+}
