1e0c423fd5c9dbd6000a317d325312c9984c0ac5
Tiberians <silverskiold@gmail.com>
2016-05-05 11:03:00 +0200

    Created tests in model

---
+++ se.biluthyrning/src/model/CardPaymentTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Silver
+ *
+ */
+public class CardPaymentTest {
+
+	/**
+	 * Test method for {@link model.CardPayment#CardPayment(model.PaymentDTO)}.
+	 */
+	@Test
+	public void testCardPayment() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.CardPayment#getApproval()}.
+	 */
+	@Test
+	public void testGetApproval() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.CardPayment#getPaymentDetails()}.
+	 */
+	@Test
+	public void testGetPaymentDetails() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ se.biluthyrning/src/model/ReceiptTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Silver
+ *
+ */
+public class ReceiptTest {
+
+	/**
+	 * Test method for {@link model.Receipt#Receipt()}.
+	 */
+	@Test
+	public void testReceipt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Receipt#Receipt(model.Payment)}.
+	 */
+	@Test
+	public void testReceiptPayment() {
+		fail("Not yet implemented");
+	}
+
+}
