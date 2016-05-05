669a7ab5682b2e785a5e700b0001b17d9df13987
Tiberians <silverskiold@gmail.com>
2016-05-05 14:46:35 +0200

    Completed CardPayment Test

--- se.biluthyrning/src/test/model/CardPaymentTest.java
+++ se.biluthyrning/src/test/model/CardPaymentTest.java
@@ -2,8 +2,11 @@
  * 
  */
 package test.model;
+import main.model.*;
 
 import static org.junit.Assert.*;
+import main.integration.Garage;
+import main.model.CardPayment;
 
 import org.junit.Test;
 
@@ -18,15 +21,22 @@ public class CardPaymentTest {
 	 */
 	@Test
 	public void testCardPayment() {
-		fail("Not yet implemented");
-	}
+		PaymentDTO paymentDTO = createPaymentDetails();
+		CardPayment cardPayment = new CardPayment(paymentDTO);
+		assertNotEquals("New CardPayment was not created", cardPayment, null);
+		}
+		
+
 
 	/**
 	 * Test method for {@link model.CardPayment#getApproval()}.
 	 */
 	@Test
-	public void testGetApproval() {
-		fail("Not yet implemented");
+	public void testGetApproval() {	
+		PaymentDTO paymentDTO = createPaymentDetails();
+		CardPayment cardPayment = new CardPayment(paymentDTO);
+		boolean paymentAuthorized = cardPayment.getApproval();
+		assertEquals("Payment was not authorized", paymentAuthorized, true);
 	}
 
 	/**
@@ -34,7 +44,24 @@ public class CardPaymentTest {
 	 */
 	@Test
 	public void testGetPaymentDetails() {
-		fail("Not yet implemented");
+		PaymentDTO paymentDTO = createPaymentDetails();
+		CardPayment cardPayment = new CardPayment(paymentDTO);
+		PaymentDTO transactionInformation = cardPayment.getPaymentDetails();
+		assertNotEquals("Can not retrieve payment details", transactionInformation, null);
+	}
+	
+	private PaymentDTO createPaymentDetails() {
+		boolean card = false;
+		int amount = 1000;
+		String cardNumber = "4894640214243468";
+		int cvc = 123;
+		int expiry = 1018;
+		int code = 1421;
+		String cardHolder = "Sten Sture";
+		
+		
+		PaymentDTO paymentDTO = new PaymentDTO(card, amount, cardNumber, cvc, expiry, code, cardHolder);
+		return paymentDTO;
 	}
 
 }
